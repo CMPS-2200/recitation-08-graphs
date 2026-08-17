31a6a7a278c25388d46cec573626deb21b3e5d59
Aaron Maus <amaus@tulane.edu>
2026-08-17 12:59:58 -0500

    Move tests to test_main

--- main.py
+++ main.py
@@ -20,24 +20,9 @@ def reachable(graph, start_node):
         break
     return result
 
-def test_reachable():
-    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
-    assert sorted(reachable(graph, 'A')) == ['A', 'B', 'C', 'D']
-
-    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
-    assert sorted(reachable(graph, 'A')) == ['A', 'B', 'C', 'D']
-    assert sorted(reachable(graph, 'E')) == ['E', 'F', 'G']
-
-
 def connected(graph):
     ### TODO
     pass
-
-def test_connected():
-    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
-    assert connected(graph) == True
-    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
-    assert connected(graph) == False
 
 def n_components(graph):
     """
@@ -46,10 +31,3 @@ def n_components(graph):
     """
     ### TODO
     pass
-
-def test_n_components():
-    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
-    assert n_components(graph) == 1
-
-    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
-    assert n_components(graph) == 2
